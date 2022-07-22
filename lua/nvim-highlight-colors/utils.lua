82b6f99d9fd95c43c8aa57da96b7752fa624fd4c
Breno Prata <brenoprata@pop-os.localdomain>
2022-07-22 23:57:23 +0200

    Add multicolor support per line

--- lua/nvim-highlight-colors/utils.lua
+++ lua/nvim-highlight-colors/utils.lua
@@ -2,6 +2,85 @@ local M = {}
 
 M.rgb_regex = "rgba?[(]+" .. string.rep("%s*%d+%s*", 3, ",") ..",?%s*%d*%.?%d*%s*[)]+"
 
+ function M.print_table(node)
+    local cache, stack, output = {},{},{}
+    local depth = 1
+    local output_str = "{\n"
+
+    while true do
+        local size = 0
+        for k,v in pairs(node) do
+            size = size + 1
+        end
+
+        local cur_index = 1
+        for k,v in pairs(node) do
+            if (cache[node] == nil) or (cur_index >= cache[node]) then
+
+                if (string.find(output_str,"}",output_str:len())) then
+                    output_str = output_str .. ",\n"
+                elseif not (string.find(output_str,"\n",output_str:len())) then
+                    output_str = output_str .. "\n"
+                end
+
+                -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+                table.insert(output,output_str)
+                output_str = ""
+
+                local key
+                if (type(k) == "number" or type(k) == "boolean") then
+                    key = "["..tostring(k).."]"
+                else
+                    key = "['"..tostring(k).."']"
+                end
+
+                if (type(v) == "number" or type(v) == "boolean") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = "..tostring(v)
+                elseif (type(v) == "table") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = {\n"
+                    table.insert(stack,node)
+                    table.insert(stack,v)
+                    cache[node] = cur_index+1
+                    break
+                else
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = '"..tostring(v).."'"
+                end
+
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                else
+                    output_str = output_str .. ","
+                end
+            else
+                -- close the table
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                end
+            end
+
+            cur_index = cur_index + 1
+        end
+
+        if (size == 0) then
+            output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+        end
+
+        if (#stack > 0) then
+            node = stack[#stack]
+            stack[#stack] = nil
+            depth = cache[node] == nil and depth + 1 or depth - 1
+        else
+            break
+        end
+    end
+
+    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+    table.insert(output,output_str)
+    output_str = table.concat(output)
+
+    print(output_str)
+end
+
 function M.get_color_value(color)
 	if (M.is_short_hex_color(color)) then
 		return M.convert_short_hex_to_hex(color)
@@ -64,6 +143,30 @@ function M.get_win_visible_rows(winid)
 	)
 end
 
+
+function M.table_find(table_param, fun)
+	for index, value in pairs(table_param) do
+		local assertion = fun(value)
+		if assertion then
+			return value
+		end
+	end
+
+	return nil
+end
+
+function M.table_filter(table_param, fun)
+	local results = {}
+	for index, value in pairs(table_param) do
+		local assertion = fun(value)
+		if assertion then
+			table.insert(results, value)
+		end
+	end
+
+	return results
+end
+
 function M.get_positions_by_regex(patterns, min_row, max_row, row_offset)
 	local positions = {}
 	local content = M.get_buffer_contents(min_row, max_row)
@@ -75,9 +178,12 @@ function M.get_positions_by_regex(patterns, min_row, max_row, row_offset)
 				local end_column = vim.fn.matchend(value, match)
 				local row = key + min_row - row_offset
 				if (row >= 0) then
+					local same_row_colors = M.table_filter(positions, function(position) return position.row == row end)
+					M.print_table({same_row_colors = same_row_colors, #same_row_colors, row = row})
 					table.insert(positions, {
 						value = match,
 						row = row,
+						display_column = #same_row_colors,
 						start_column = start_column,
 						end_column = end_column
 					})
@@ -106,7 +212,8 @@ function M.create_window(row, col, bg_color)
 	vim.api.nvim_win_set_option(window, 'winhighlight', 'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name)
 
 	local row_content = M.get_buffer_contents(row + 1, row + 2)
-	vim.api.nvim_buf_set_lines(buf, 0, 0, true, {string.sub(row_content[1], 0, 1)})
+	local col_position_on_buffer = col == 0 and 1 or col + 1
+	vim.api.nvim_buf_set_lines(buf, 0, 0, true, {string.sub(row_content[1], col_position_on_buffer, col_position_on_buffer)})
 
 	return window
 end
