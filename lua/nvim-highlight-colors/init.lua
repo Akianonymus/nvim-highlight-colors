26ceb90367e127bac1c14a44e6c9dd8f07884199
Breno Prata <brenoprata@hotmail.com>
2022-07-24 15:12:40 +0200

    Showing multiple colors with highlight logic

--- lua/nvim-highlight-colors/init.lua
+++ lua/nvim-highlight-colors/init.lua
@@ -3,88 +3,33 @@ local colors= require("nvim-highlight-colors.colors")
 
 local load_on_start_up = false
 local row_offset = 2
-local windows = {}
 
-function is_window_already_created(row, value)
-	for _, windows_data in ipairs(windows) do
-		if windows_data.row == row and value == windows_data.color then
-			return true
-		end
-	end
-
-	return false
+function clear_highlights()
+	vim.api.nvim_buf_clear_namespace(0, -1, 0, utils.get_last_row_index())
 end
 
-function close_windows()
-	local ids = {}
-	for _, window_data in ipairs(windows) do
-		table.insert(ids, window_data.win_id)
-	end
-	utils.close_windows(ids)
-	windows = {}
-end
-
-function close_not_visible_windows(min_row, max_row)
-	local windows_to_remove = {}
-	local new_windows_table = {}
-	for _, window_data in ipairs(windows) do
-		local window_config = vim.api.nvim_win_get_config(window_data.win_id)
-		local window_bufpos = window_config.bufpos
-		local window_row = window_bufpos[1] + row_offset
-		local is_visible = window_row <= max_row and window_row >= min_row
-		if is_visible == false then
-			table.insert(windows_to_remove, window_data.win_id)
-		else
-			table.insert(new_windows_table, window_data)
-		end
-	end
-	utils.close_windows(windows_to_remove)
-	windows = new_windows_table
-end
-
-function show_visible_windows(min_row, max_row)
+function create_color_highlights(min_row, max_row)
 	local positions = utils.get_positions_by_regex(
 		{
 			colors.hex_regex,
 			colors.rgb_regex
 		},
-		min_row - 1,
+		min_row,
 		max_row,
 		row_offset
 	)
 	for _, data in pairs(positions) do
-		if is_window_already_created(data.row, data.value) == false then
-			table.insert(
-				windows,
-				{
-					win_id = utils.create_window(data.row, 0, data.value),
-					row = data.row,
-					color = data.value
-				}
-			)
-		end
+		utils.create_highlight_range(data.row, data.display_column, data.value)
 	end
 end
 
-function update_windows_visibility()
-	local visible_rows = utils.get_win_visible_rows(0)
-	local min_row = visible_rows[1]
-	local max_row = visible_rows[2]
-
-	show_visible_windows(min_row, max_row)
-	close_not_visible_windows(min_row, max_row)
-end
-
 function turn_on()
-	close_windows()
-	local visible_rows = utils.get_win_visible_rows(0)
-	local min_row = visible_rows[1]
-	local max_row = visible_rows[2]
-	show_visible_windows(min_row, max_row)
+	clear_highlights()
+	create_color_highlights(0, utils.get_last_row_index())
 end
 
 function turn_off()
-	close_windows()
+	clear_highlights()
 end
 
 function setup()
@@ -92,11 +37,7 @@ function setup()
 end
 
 vim.api.nvim_create_autocmd({"TextChanged", "TextChangedI", "TextChangedP", "VimResized"}, {
-	callback = turn_on,
-})
-
-vim.api.nvim_create_autocmd({"WinScrolled"}, {
-	callback = update_windows_visibility,
+       callback = turn_on,
 })
 
 vim.api.nvim_create_autocmd({"BufEnter"}, {
--- lua/nvim-highlight-colors/utils.lua
+++ lua/nvim-highlight-colors/utils.lua
@@ -1,6 +1,11 @@
 local colors = require("nvim-highlight-colors.colors")
+local table_utils = require("nvim-highlight-colors.table_utils")
 
 local M = {}
+
+function M.get_last_row_index()
+	return vim.fn.line('$')
+end
 
 function M.get_buffer_contents(min_row, max_row)
 	return vim.api.nvim_buf_get_lines(0, min_row, max_row, false)
@@ -40,33 +45,22 @@ function M.get_positions_by_regex(patterns, min_row, max_row, row_offset)
 		end
 	end
 
+	table.sort(positions, function (position1, position2) return position1.row == position2.row and position1.start_column < position2.start_column end)
+
+	for _, position in ipairs(positions) do
+		local same_row_colors = table_utils.filter(positions, function(pos) return pos.row == position.row end)
+		local match_index = table_utils.find_index(same_row_colors, function (pos) return pos.row == position.row and pos.start_column == position.start_column end)
+		position.display_column = match_index - 1
+	end
+
+	table_utils.print({positions})
 	return positions
 end
 
-function M.create_window(row, col, bg_color)
+function M.create_highlight_range(row, col, bg_color)
 	local highlight_color_name = string.gsub(bg_color, "#", ""):gsub("[(),%s%.]+", "")
-	local buf = vim.api.nvim_create_buf(false, true)
-	local window = vim.api.nvim_open_win(buf, false, {
-		relative = "win",
-		bufpos={row, col},
-		width = 1,
-		height = 1,
-		focusable = false,
-		noautocmd = true,
-		zindex = 1,
-		style= "minimal"
-	})
 	vim.api.nvim_command("highlight " .. highlight_color_name .. " guibg=" .. colors.get_color_value(bg_color))
-	vim.api.nvim_win_set_option(
-		window,
-		'winhighlight',
-		'Normal:' .. highlight_color_name .. ',FloatBorder:' .. highlight_color_name
-	)
-
-	local row_content = M.get_buffer_contents(row + 1, row + 2)
-	vim.api.nvim_buf_set_lines(buf, 0, 0, true, {string.sub(row_content[1], 0, 1)})
-
-	return window
+	vim.api.nvim_buf_add_highlight(0, -1, highlight_color_name, row + 1, col, col + 1)
 end
 
 
